d650938072a88a49f23328236c12c2d2b1f660c7
ZackCodeCrypto <s30846@pjwstk.edu.pl>
2025-04-14 21:38:52 +0200

    Initial commit - full LINQ solution with tests

--- RESTApi/Models/Animal.cs
+++ RESTApi/Models/Animal.cs
@@ -4,7 +4,7 @@ public class Animal
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Category { get; set; } // e.g., dog, cat
+    public string Category { get; set; }
     public double Weight { get; set; }
     public string FurColor { get; set; }
 }
